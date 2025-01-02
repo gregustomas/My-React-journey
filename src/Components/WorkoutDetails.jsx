9def0e9ab4a75dee5fcd58ec84610d8722497ef6
Tomáš Greguš <159766435+gregustomas@users.noreply.github.com>
2025-01-02 20:57:08 +0100

    add workout creator

--- src/Components/WorkoutDetails.jsx
+++ src/Components/WorkoutDetails.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom'
 
 export function WorkoutDetails() {
 
     const { workoutName } = useParams();
+    const [ exercises, setExercises ] = useState();
+    const sets =document.getElementById("sets");
+
+
 
     return(
         <>
         <h2>Workout deatails - { workoutName }</h2>
+        <form id='container'>
+            Serie:
+            <input type="number" id='sets'/>
+            <button type="submit">Add exercise</button>
+        </form>
         </>
     )
 }
